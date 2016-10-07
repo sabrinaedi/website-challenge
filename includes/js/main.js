d930ef41a791207e72bc7078c152b5c256b938ba
noémie courtois <courtoisno@gmail.com>
2016-10-07 16:40:56 +0200

    changes made to contact

--- includes/js/main.js
+++ includes/js/main.js
@@ -148,23 +148,11 @@ function showScores ( ) {
 
 /*NONO CHANGES -> GOOGLE MAPS */
 
-$(function () {
-
-    function initMap() {
-
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
-
-        var mapCanvas = document.getElementById('myMap');
-        
-        var mapOptions = {
-            center: location,
-            zoom: 16,
-            panControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-
-    }
-
-    google.maps.event.addDomListener(window, 'load', initMap);
-});
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          scrollwheel: false,
+          zoom: 8
+        });
+      }
