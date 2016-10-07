754d33029d98b03cd5dee6c0bddcfdfe5b337133
Sabedi <nsab@edinger.org>
2016-10-07 18:06:40 +0200

    reshaped layout etc, trying around with the movign buttons...

--- includes/js/jquery-1.10.2.js
+++ includes/js/jquery-1.10.2.js
@@ -9787,3 +9787,15 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 }
 
 })( window );
+
+$('.touchMeNot').on('mouseenter',function(e){
+    var maxX = $(window).width() - $(this).width();
+    var maxY = $(window).height() - $(this).height();    
+    $(this).css({
+        'left':getRandomInt(0, maxX),
+        'top':getRandomInt(0, maxY)
+    });
+});
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- includes/js/main.js
+++ includes/js/main.js
@@ -143,3 +143,16 @@ function showScores ( ) {
     $ ('#score').html("<h2 id='score'>... the Perfect 10 Godlike </h2>")
   }
 }
+
+$('.touchMeNot').on('mouseenter',function(e){
+    var maxX = $(window).width() - $(this).width();
+    var maxY = $(window).height() - $(this).height();    
+    $(this).css({
+        'left':getRandomInt(0, maxX),
+        'top':getRandomInt(0, maxY)
+    });
+});
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
