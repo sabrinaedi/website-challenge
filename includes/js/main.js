14a9b5f1d4347607dae82672fc53783b7ab719f7
noémie courtois <courtoisno@gmail.com>
2016-10-07 14:31:47 +0200

    Contact page styled

--- includes/js/main.js
+++ includes/js/main.js
@@ -143,3 +143,28 @@ function showScores ( ) {
     $ ('#score').html("<h2 id='score'>... the Perfect 10 Godlike </h2>")
   }
 }
+
+
+
+/*NONO CHANGES -> GOOGLE MAPS */
+
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+
+        var mapCanvas = document.getElementById('myMap');
+        
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
