d677c3c7f44f14e50320bd839099269f7c0e5697
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-09 01:38:50 +0200

    manually updated the main.js file

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-$(document).ready(function(){
-  // Initialize Tooltip
-  $('[data-toggle="tooltip"]').tooltip();
-  
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-})
-
 // Initial set up to hide the elements
 $(document).ready(function() {
   $( '.buttons' ).hide()
@@ -173,13 +144,7 @@ function showScores ( ) {
   }
 }
 
-/*NONO CHANGES -> GOOGLE MAPS */
-
-function initMap() {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 8
-        });
-      }
+$('#trybutton').click(function() {
+    $("#test").html('The text has now changed!')
+    $("#test").animate({left: '100px', top: '100px'},500)
+})
