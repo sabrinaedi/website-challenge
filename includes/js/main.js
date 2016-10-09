7379e2fb31dec8cbbf32e5770b0dec2692a02160
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-09 02:02:54 +0200

    added the dynamics scrolling code from the original template

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,34 +1,30 @@
-// $(document).ready(function(){
-//   // Initialize Tooltip
-//   $('[data-toggle="tooltip"]').tooltip();
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
   
-//   // Add smooth scrolling to all links in navbar + footer link
-//   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
 
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-//       // Store hash
-//       var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 900, function(){
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
    
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// })
-
-// Initial set up to hide the elements
-$(document).ready(function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  })
   $( '.buttons' ).hide()
   $( '#btn0' ).hide()
   $( '#btn1' ).hide()
@@ -38,7 +34,21 @@ $(document).ready(function() {
   $( '#choice2' ).hide()
   $( '#question' ).hide()
 
+// });
 })
+
+// Initial set up to hide the elements
+// $(document).ready(function() {
+  // $( '.buttons' ).hide()
+  // $( '#btn0' ).hide()
+  // $( '#btn1' ).hide()
+  // $( '#btn2' ).hide()
+  // $( '#choice0' ).hide()
+  // $( '#choice1' ).hide()
+  // $( '#choice2' ).hide()
+  // $( '#question' ).hide()
+
+// })
 // #start-button
  $( ".fa-play" ).click(function( ){
   $( ".fa-play" ).fadeOut(500)
