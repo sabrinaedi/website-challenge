2dede765d15b17c61cda79fa3ba6e42b3b8980ab
Sabedi <nsab@edinger.org>
2016-10-10 09:22:40 +0200

    tiny correction in running function

--- includes/js/main.js
+++ includes/js/main.js
@@ -145,7 +145,7 @@ function showScores ( ) {
 }
 
 
-$('#runner', '#runnerB', '#runnerC').on('mouseover', function(){
+$('#runner, #runnerB, #runnerC').on('mouseover', function(){
     var offset = $(this).offset();
     var goX = Math.random() < 0.5 ? -1 : 1;
     var goY = Math.random() < 0.5 ? -1 : 1;
